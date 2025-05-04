a6e950a993290e5cd8d639dfe6f52cfad8942bbc
rakib hasan <rakibhasanmd457@gmail.com>
2025-05-04 13:09:41 +0600

    add array undastanding

--- test.js
+++ test.js
@@ -1,3 +1,12 @@
-const arr = [4, 5, 6, 7,'hasan']
+const arr = [4, 5, 6, 7, 'hasan']
+
 const arr2 = [0, 1, 2, 3, ...arr, 8, 9]
+
 console.log(arr2);
+
+const arr3 = [...arr]
+const submit = arr3.filter(e => e > 5)
+const submit2 = arr3.map(e => e * 3)
+
+console.log('the result is', submit);
+console.log('the map is multipal', submit2);
