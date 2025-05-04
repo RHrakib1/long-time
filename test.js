76fe7d4d27aabe5a1eeaa58c0cd1903aad60ac01
rakib hasan <rakibhasanmd457@gmail.com>
2025-05-04 13:11:32 +0600

    adding another

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ console.log(arr2);
 
 const arr3 = [...arr]
 const submit = arr3.filter(e => e > 5)
-const submit2 = arr3.map(e => e * 3)
+const submit2 = submit.map(e => e * 3)
 
 console.log('the result is', submit);
 console.log('the map is multipal', submit2);
