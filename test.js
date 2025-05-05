f7f3f41493b756d1d9c513a879cbf84907328d95
rakib hasan <rakibhasanmd457@gmail.com>
2025-05-05 22:59:10 +0600

    array method done

--- test.js
+++ test.js
@@ -1,4 +1,16 @@
 const number = [1, 2, 3, 4, 5, 6]
-const newmultipal = number.map(e => e * 2)
+// const newmultipal = number.map(e => e * 2)
+const filt = number.filter(e => e >= 4)
+const another = filt.map(e => e * 3)
+const khoja = number.find(e => e >= 2)
+const new1 = number.reduce((total, bill) => total + bill)
+const new2 = number.includes(1)
 
-console.log(newmultipal);
+console.log(new2);
+
+// console.log(newmultipal);
+// console.log(filterr);
+console.log(filt);
+console.log(another);
+console.log(khoja);
+console.log(new1);
