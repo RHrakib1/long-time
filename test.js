63b01aec2ed4e9bc9cb19458a6818390989fc8a8
rakib hasan <rakibhasanmd457@gmail.com>
2025-05-05 20:36:38 +0600

    add map and foreach

--- test.js
+++ test.js
@@ -1,12 +1,4 @@
-const arr = [4, 5, 6, 7, 'hasan']
+const number = [1, 2, 3, 4, 5, 6]
+const newmultipal = number.map(e => e * 2)
 
-const arr2 = [0, 1, 2, 3, ...arr, 8, 9]
-
-console.log(arr2);
-
-const arr3 = [...arr]
-const submit = arr3.filter(e => e > 5)
-const submit2 = submit.map(e => e * 3)
-
-console.log('the result is', submit);
-console.log('the map is multipal', submit2);
+console.log(newmultipal);
